eef5a1f3b836a74caf5739e2c55350232a5b89fb
TREYCYBRY <2022210212@udh.edu.pe>
2025-05-22 23:13:08 -0500

    Desarrollo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlatoComponent } from './plato/plato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlatoComponent
   ],
   imports: [
     BrowserModule,
